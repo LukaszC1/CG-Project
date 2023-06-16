10b7c5262820979cdc2d7a6d0a57bc0cf75b0f94
Mateusz Kies <lumios100@gmail.com>
2023-06-16 22:26:41 +0200

    boss enemies

--- RogueLike/Assets/Scripts/BossItemPickUp.cs
+++ RogueLike/Assets/Scripts/BossItemPickUp.cs
@@ -6,13 +6,6 @@ using UnityEngine;
 
 public class BossItemPickUp : MonoBehaviour, iPickUpObject
 {
-    
-    private float speed = 2.3f;
-    private float speed2 = 3;
-    Transform targetDestination;
-    private float timer = 0.2f;
-
-
 
     public void OnPickUp(Character character)
     {
@@ -31,28 +24,8 @@ public class BossItemPickUp : MonoBehaviour, iPickUpObject
 
         Destroy(gameObject);
     }
-
-    private void Update()
-    {
-        if (targetDestination != null)
-        {
-            timer -= Time.deltaTime;
-            if (timer >= 0)
-            {
-                Vector3 direction = (targetDestination.position - transform.position).normalized;
-                transform.position -= speed2 * Time.deltaTime * direction.normalized;
-                speed2 *= 0.99f;
-            }
-            else
-            {
-                Vector3 direction = (targetDestination.position - transform.position).normalized;
-                transform.position += speed * Time.deltaTime * direction.normalized;
-                speed *= 1.001f;
-            }
-        }
-    }
     public void setTargetDestination(Transform destination)
     {
-        targetDestination = destination;
+     
     }
 }
